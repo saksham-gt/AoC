87065c40f3a9db9ea72ff8363a7e64203d791490
Saksham Gupta <55953830+glitchpop-frenzy@users.noreply.github.com>
2024-09-26 17:35:01 +0530

    task: Day01

--- src/Day01.kt
+++ src/Day01.kt
@@ -1,17 +1,34 @@
 fun main() {
-    fun part1(input: List<String>): Int {
-        return input.size
-    }
+    val calories : MutableList<Int> = mutableListOf<Int>()
+
 
     fun part2(input: List<String>): Int {
-        return input.size
+        return calories[0] + calories[1] + calories[2]
+    }
+
+    fun part1(input: List<String>): Int {
+        var elfCalorie: Int = 0;
+        for(str in input){
+            if (str!="\n" && str!=""){
+                elfCalorie += str.toInt()
+                continue;
+            } else {
+                calories.add(elfCalorie)
+                elfCalorie = 0
+            }
+        }
+        calories.sortDescending()
+
+        return calories[0]
     }
 
     // test if implementation meets criteria from the description, like:
     val testInput = readInput("Day01_test")
-    check(part1(testInput) == 1)
 
     val input = readInput("Day01")
     part1(input).println()
     part2(input).println()
+
+
+
 }
